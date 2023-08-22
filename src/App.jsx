9ff3cd168206c5c3024859b6578d07bf4ca13b67
naresh-vunnam77 <77763333+naresh-vunnam77@users.noreply.github.com>
2023-08-22 17:32:52 +0530

    template commit
    initial commit for designing a vite react template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <section className="flex bg-gray-900 text-white min-h-screen min-w-screen items-center justify-center align-middle ">
+      <div>Hello world </div>
+    </section>
+  )
+}
+
+export default App
